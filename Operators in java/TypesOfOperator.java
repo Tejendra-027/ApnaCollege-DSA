8d7bcb03ae5b8a7761766fa46082610e7b82670c
Tejendra-027 <tejendra.22jics027@jietjodhpur.ac.in>
2025-04-30 12:55:04 +0530

    create a folder

---
+++ Operators in java/TypesOfOperator.java
@@ -0,0 +1,16 @@
+                // # Types of Operators
+                /* 
+                1. Arithmetic Operators (Binary/Unary)
+                2. Relational Operators
+                3. Logical Operators
+                4. Bitwise Operators
+                5. Assignment Operators
+                */
+
+
+
+public class TypesOfOperator {
+    public static void main(String[] args) {
+
+    }
+}
